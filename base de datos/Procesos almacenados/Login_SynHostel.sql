3d48dbe5e8697854560b3d21b71147fe7a234549
ivancillo <ivan.a.zamorano.c@gmail.com>
2015-05-23 13:10:04 -0300

    [Updated] SP: Login

--- base de datos/Procesos almacenados/Login_SynHostel.sql
+++ base de datos/Procesos almacenados/Login_SynHostel.sql
@@ -1,46 +1,42 @@
-create or replace procedure synhostel.login(
+create or replace procedure synchostel.login(
     in rut varchar,
     in password varchar,
-    out out_id_perfil integer,
-    out out_mail_persona varchar,
-    out out_nombre varchar,
+    out personas refcursor,
     out num_error varchar, 
     out mensaje_error varchar
 ) returns record as
 
 $body$
 
-    declare _id_perfil          integer;
-    declare _mail               varchar(100);
-    declare _nombre             varchar(100);
-    
+begin
+    numerror := 0;
+    msjerror := ' ';
     begin
-        num_error := 0;
-        mensaje_error := ' ';
+        open personas for
 
-        select
-            perfil.id_perfil,
-            persona.mail,
-            persona.nombre,
-        into
-            _id_perfil,
-            _mail,
-            _nombre,
-        from
-            usuario, persona
-        where
-            perfil.rut = rut and  persona.password = password;
+            select
+                rut,
+                nombre,
+                email,
+                password,
+                telefono,
+                telefono_2,
+                direccion,
+                admin,
+                fecha_nacimiento,
+                fecha_ingreso_hostal
+            from 
+                persona
+            where
+                persona.rut = rut and persona.password = password;
 
-        if found then
-        out_id_perfil := _id_perfil;
-        out_mail_persona := _mail;
-        out_nombre := _nombre;
-        return;
-
-        else
-        num_error := '1';
-        mensaje_error := 'Datos de inicio de sesión inválidos.';
-        end if;
+            exception
+                when others then
+                    numerror := sqlstate;
+                    msjerror := '[carga_usuarios] error al cargar usuario(sql) ' ||sqlerrm;
+                    return; 
     end;
+
+end;
 $body$
 language 'plpgsql'
