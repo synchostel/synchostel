8a570a2692e6ca674d73e18587db1e2fca18e28c
ivancillo <ivan.a.zamorano.c@gmail.com>
2015-05-22 23:05:02 -0300

    [Updated] Persona
    Se agrega el campo password

--- base de datos/tablas/persona.sql
+++ base de datos/tablas/persona.sql
@@ -5,6 +5,7 @@ CREATE TABLE persona(
 	rut varchar(9) primary key, -- 99999999k
 	nombre varchar(100),
 	email varchar(100),
+	password varchar(20),
 	telefono int,
 	telefono_2 int,
 	direccion varchar(300),
