c4acf1bee170f1975b7daade4f89cbe8f000b047
Roberto <roberto.osvaldo.c@gmail.com>
2015-05-28 02:03:08 -0300

    Commitie con la wea abierta, BD AGAIN

--- base de datos/tablas/script creacion BD.sql
+++ base de datos/tablas/script creacion BD.sql
@@ -16,6 +16,7 @@ CREATE TABLE tarea(
 	prioridad varchar(5) --(alta,media,baja)
 );
 
+
 CREATE TABLE permiso(
 	id serial primary key,
 	nombre varchar(50),
@@ -27,7 +28,7 @@ CREATE TABLE huesped(
 	nombre varchar(100),
 	nacionalidad varchar(50),
 	vegano boolean,
-	celiaco boolean,
+	celiaco boolean
 );
 
 CREATE TABLE servicio(
@@ -59,25 +60,25 @@ CREATE TABLE bloque_horario(
 	id serial primary key,
 	inicio_bloque timestamp,
 	termino_bloque timestamp,
-	fecha timestamp references dia(fecha),
+	fecha timestamp references dia(fecha)
 );
 
 CREATE TABLE bloque_asignado(
-	id serial primary key,
+	id serial,
 	run varchar(9), -- 99999999k
 	id_blq_horario serial,
-	primary key (run, id_blq_horario),
-	CONSTRAINT FK_persona foreing key (run) references persona(run) ON DELETE CASCADE,
-	CONSTRAINT FK_bloque_horario foreing key (id_blq_horario) references bloque_horario(id) ON DELETE CASCADE
+	primary key (id,run, id_blq_horario),
+	CONSTRAINT FK_persona FOREIGN KEY (run) references persona(run) ON DELETE CASCADE,
+	CONSTRAINT FK_bloque_horario FOREIGN KEY (id_blq_horario) references bloque_horario(id) ON DELETE CASCADE
 );
 
 CREATE TABLE cantidad_de_permisos(
-	id serial primary key,
+	id serial,
 	id_perfil serial,
 	id_permiso serial,
-	primary key (id_permiso, id_perfil),
-	CONSTRAINT FK_perfi foreing key (id_perfil) references perfil(id) ON DELETE CASCADE,
-	CONSTRAINT FK_permiso foreing key (id_permiso) references permiso(id) ON DELETE CASCADE
+	primary key (id, id_permiso, id_perfil),
+	CONSTRAINT FK_perfi FOREIGN KEY (id_perfil) references perfil(id) ON DELETE CASCADE,
+	CONSTRAINT FK_permiso FOREIGN KEY (id_permiso) references permiso(id) ON DELETE CASCADE
 );
 
 CREATE TABLE producto(
@@ -86,7 +87,7 @@ CREATE TABLE producto(
 	cantidad int,
 	cantidad_critica int,
 	categoria varchar(50),
-	rut varchar(9) references hostal(rut) ON DELETE CASCADE -- 99999999k
+	run varchar(9) references hostal(run) ON DELETE CASCADE -- 99999999k
 );
 
 CREATE TABLE pieza(
@@ -99,7 +100,7 @@ CREATE TABLE pieza(
 	caja_fuerte boolean,
 	copa boolean, --copas de vino
 	calefactor boolean,
-	id_hostal id serial references hostal(id) ON DELETE CASCADE
+	run_hostal varchar(9) references hostal(run) ON DELETE CASCADE
 );
 
 CREATE TABLE gasto(
@@ -107,20 +108,20 @@ CREATE TABLE gasto(
 	descripcion varchar(100),
 	monto int,
 	fecha_gasto timestamp,
-	run_hostal references hostal(run)
+	run_hostal varchar(9) references hostal(run)
 );
 
 CREATE TABLE cama(
 	id serial primary key,
 	descripcion varchar(100),
 	valor int,
-	id serial references pieza(id) ON DELETE CASCADE
+	id_pieza serial references pieza(id) ON DELETE CASCADE
 );
 
 CREATE TABLE telefonos(
-	rut varchar(9) references persona(rut) ON DELETE CASCADE,
+	run varchar(9) references persona(run) ON DELETE CASCADE,
 	telefono int,
-	primary key (rut, telefono)
+	primary key (run, telefono)
 );
 
 CREATE TABLE reserva(
@@ -129,31 +130,31 @@ CREATE TABLE reserva(
 	fecha_check_out timestamp,
 	medio_pago varchar(100),
 	mapa boolean,
-	pasaporte references huesped(pasaporte) ON DELETE CASCADE, 
-	id_hostal references hostal(id) ON DELETE CASCADE
+	pasaporte varchar(50) references huesped(pasaporte) ON DELETE CASCADE, 
+	run_hostal varchar(9) references hostal(run) ON DELETE CASCADE
 );
 
 CREATE TABLE cantidad_de_camas(
-	id serial primary key,
+	id serial,
 	id_cama serial, 
 	id_reserva serial, 
-	primary key (id_reserva,id_cama),
-	CONSTRAINT FK_cama foreing key (id_cama) references cama(id) ON DELETE CASCADE,
-	CONSTRAINT FK_reserva foreing key (id_reserva) references reserva(id) ON DELETE CASCADE
+	primary key (id, id_reserva,id_cama),
+	CONSTRAINT FK_cama FOREIGN KEY (id_cama) references cama(id) ON DELETE CASCADE,
+	CONSTRAINT FK_reserva FOREIGN KEY (id_reserva) references reserva(id) ON DELETE CASCADE
 );
 
 CREATE TABLE boleta(
 	id serial primary key,
 	total int,
 	estado_de_pago boolean,
-	id serial references reserva(id) ON DELETE CASCADE
+	id_reserva serial references reserva(id) ON DELETE CASCADE
 );
 
 CREATE TABLE cantidad_de_servicios(
-	id serial primary key,
+	id serial,
 	id_boleta serial,
 	id_servicio serial,
-	primary key(id_servicio, id_boleta),
-	CONSTRAINT FK_servicio foreing key (id_servicio) references servicio(id) ON DELETE CASCADE,
-	CONSTRAINT FK_boleta foreing key (id_boleta) references boleta(id) ON DELETE CASCADE
+	primary key(id, id_servicio, id_boleta),
+	CONSTRAINT FK_servicio FOREIGN KEY (id_servicio) references servicio(id) ON DELETE CASCADE,
+	CONSTRAINT FK_boleta FOREIGN KEY (id_boleta) references boleta(id) ON DELETE CASCADE
 );
